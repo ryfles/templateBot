54ba0d7654d5bfa4db8f7219bcf513b716a447df
Lenny <90214734+ryfles@users.noreply.github.com>
2023-07-31 23:45:28 +0200

    Update PingCommand.java

--- src/main/java/de/ryfle/templatebot/commands/PingCommand.java
+++ src/main/java/de/ryfle/templatebot/commands/PingCommand.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class PingCommand extends ListenerAdapter {
 
     public void onSlashCommandInteraction(SlashCommandInteractionEvent e) {
+        //reacts to the event and checks if the command was /ping
         if(e.getName().equals("ping")) {
             e.replyEmbeds(embedCreator.defaultEmbed("Pong!!", "The bot has reacted to your command")).queue();
         }
