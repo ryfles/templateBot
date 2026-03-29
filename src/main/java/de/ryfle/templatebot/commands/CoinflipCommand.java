c9c79e76dff1ba984bb15e6886be79ee8578d258
lennyvy <lenny.hepbeceren@gmail.com>
2026-03-29 13:05:50 +0200

    Improved the DiceRoll Command
    - Improved the Command itself for better visualtiy
    - Added various comments to give the reader a better understanding

--- src/main/java/de/ryfle/templatebot/commands/CoinflipCommand.java
+++ src/main/java/de/ryfle/templatebot/commands/CoinflipCommand.java
@@ -6,19 +6,22 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.util.Random;
 
-public class CoinflipCommand extends ListenerAdapter {
+//here are some variables that one can change on the fly
+private int diceSides = 6;
+public int result;
+public string embedHeader = "The result of your dice roll is...";
+public string embedBody = "You rolled a " + result + "!";S
 
+//command classes have to extend ListernerAdapter to be registered as such
+public class DiceRollCommand extends ListenerAdapter {
+
+    //void function onSlashCommandInteraction is more common these day 
     public void onSlashCommandInteraction(SlashCommandInteractionEvent e) {
-        if(e.getName().equals("coinflip")) {
-            //gives out a either 1 or 0, if 0 the coin says heads, if 1 the coin says tails
+        if(e.getName().equals("DiceRoll")) {
+            //the two following rows generate the output of the dice roll
             Random r = new Random();
-            int result = r.nextInt(2);
-            if(result == 0) {
-                e.replyEmbeds(embedCreator.defaultEmbed("The result of the coinflip is...", "You got heads!")).queue();
-            } else {
-                e.replyEmbeds(embedCreator.defaultEmbed("The result of the coinflip is...", "You got tails!")).queue();
-            }
+            int result = r.nextInt(diceSides) + 1; //the dice roll has to be counted +1 since if diceSides is set to 6 the result could be from 0 to 5. the functions always starts with 0 as its first number
+            e.replyEmbeds(embedCreator.defaultEmbed(embedHeader, embedBody)).queue();
         }
     }
-
 }
